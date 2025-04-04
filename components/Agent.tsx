fe118f01d27a73299fbc7fea7b980c45f18269c9
Mileslegend <mileslegend7@gmail.com>
2025-04-04 13:18:43 +0000

    confused

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { vapi } from "@/lib/vapi.sdk";
 import { interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -69,6 +70,13 @@ const Agent = ({
       success: true,
       id: "feedback-id",
     };
+
+    const { success, feedbackId } = await createFeedback({
+      interviewId: interviewId!,
+      userId: userId!,
+      transcript: messages
+    })
+
     if (success && id) {
       router.push(`/interview/${interviewId}/feedback`);
     } else {
