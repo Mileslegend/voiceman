7dd9b03011490fc4887f53269179387dfb06a00f
Mileslegend <mileslegend7@gmail.com>
2025-04-04 10:11:00 +0000

    implement interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import {
-  getCurrentUser,
   getInterviewsByUserId,
   getLatestInterviews,
-} from "@/lib/actions/auth.action";
+} from "@/lib/actions/general.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -19,7 +18,7 @@ const Home = async () => {
   ]);
 
   const hasPastInterviews = userInterviews?.length > 0;
-  const hasUpcomingInterviews = latestInterviews?.length > 0
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
 
   return (
     <>
@@ -58,7 +57,7 @@ const Home = async () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-        {hasUpcomingInterviews ? (
+          {hasUpcomingInterviews ? (
             latestInterviews?.map((interview) => (
               <InterviewCard {...interview} key={interview.id} />
             ))
